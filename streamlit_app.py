3b407ffb4289dd39e946c253eead0c0beb4e32d8
flyingsalmon <tanman1129@hotmail.com>
2024-10-29 21:17:01 +0000

    added map

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,19 +1,20 @@
-import streamlit as st
-import numpy as np
-import pandas as pd
-
+#import streamlit as st
+#import numpy as np
+#import pandas as pd
+'''
 chart_data = pd.DataFrame(
      np.random.randn(20, 3),
      columns=['a', 'b', 'c'])
 
 st.line_chart(chart_data)
+'''
 
 import streamlit as st
 import numpy as np
 import pandas as pd
 
 map_data = pd.DataFrame(
-    np.random.randn(1000, 2) / [50, 50] + [47.86, -122.2],
+    np.random.randn(100, 2) / [50, 50] + [47.86, -122.2],
     columns=['lat', 'lon'])
 
 st.map(map_data)
